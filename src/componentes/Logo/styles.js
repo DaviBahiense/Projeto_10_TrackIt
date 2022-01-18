759c9993848e3938298cfda8084975e0f8c8e06e
mrcs22 <marcusmoraes2010@hotmail.com>
2022-01-18 01:23:07 -0300

    Preparando revisão

--- src/componentes/Logo/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Conteudo = styled.div`
-    width: 180px;
-    height: 178px;
-    margin-top: 68px;
-    margin-botton: 32px;
-`
-export {Conteudo} 
